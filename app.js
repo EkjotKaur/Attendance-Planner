ffe65769676e1ac06cbe3c229f1841dad09e59bd
Ekjot Kaur <ekjotkaurk13@gmail.com>
2020-11-12 18:20:35 +0530

    DB setup

--- app.js
+++ app.js
@@ -3,15 +3,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mongoose.connect('mongodb://localhost:27017/classDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
+const batchSchema = new mongoose.Schema({
+  tchName: String,
+  email: String,
+  batch: String,
+  subject: String,
+  password: String
+});
+
+const Batch = mongoose.model('Batch', batchSchema);
 
 app.get('/', (req, res) => {
   res.render("index");
@@ -23,6 +35,24 @@ app.get('/login', (req, res) => {
 
 app.get('/signup', (req, res) => {
   res.render('signup');
+});
+
+app.post('/signup', function(req, res){
+  const newBatch = new Batch({
+    tchName: req.body.teacher,
+    email: req.body.email,
+    batch: req.body.batch,
+    subject: req.body.subject,
+    password: req.body.password
+  });
+
+  newBatch.save(err => {
+    if(err){
+      console.log(err);
+    } else {
+      res.render("index");
+    }
+  });
 });
 
 
