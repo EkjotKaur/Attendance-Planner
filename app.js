d7b31a396b4d4ef0cc22cb22a6414f9e0cc9b342
Ekjot Kaur <ekjotkaurk13@gmail.com>
2020-11-19 13:35:20 +0530

    backend sorted

--- app.js
+++ app.js
@@ -202,9 +202,28 @@ app.get("/:presentClassId/:presentBatchId/attendance", (req, res) => {
                     totalDays: 0,
                     present: 0,
                   });
+                  
                   record.save();
+                  console.log(record);
                 } else {
                   // console.log(foundRecord.totalDays);
+                  if(Month!=foundRecord.month){
+                      Attendance.findOneAndDelete({_id: foundRecord.id}, (err, delAtt) => {
+                        if(err){
+                          console.log(err);
+                        } else {
+                          console.log(delAtt);
+                          const record = new Attendance({
+                            month: Month,
+                            stdId: studentItem[i].id,
+                            classId: req.params.presentClassId,
+                            totalDays: 0,
+                            present: 0,
+                          });
+                          record.save();
+                        }
+                      });
+                  }
                   if(foundRecord.Days.length>0 && foundRecord.totalDays>0){
                     // console.log(foundRecord.totalDays);
                     // console.log("ABC"+foundRecord.Days[0]);
@@ -212,6 +231,7 @@ app.get("/:presentClassId/:presentBatchId/attendance", (req, res) => {
                     dd = new Date().getDate();
                     // console.log("DD"+dd);
                     // console.log("fr"+foundRecord.Days[index]);
+                    
                     if(foundRecord.Days[index]==dd){
                       console.log("Already Submitted");
                     }
@@ -405,7 +425,7 @@ app.post("/newclass", (req, res) => {
             userId: req.user.id,
             slotId: foundShift.id,
           });
-
+          
           batch.save();
           res.redirect("home");
         }
@@ -429,13 +449,27 @@ app.post("/:presentClassId/:presentBatchId/updateClass", (req, res) => {
 });
 
 app.post("/:presentClassId/:presentBatchId/deleteClass", (req, res) => {
+  // Class.findOne({_id: req.params.presentClassId}, (err, foundC) => {
+  //   console.log(foundC);
+  // });
   Class.findByIdAndDelete(
     { _id: req.params.presentClassId },
     (err, deletedClass) => {
       if (err) {
         console.log(err);
       } else {
-        res.redirect("/home");
+        console.log(deletedClass);
+        // Attendance.find({classId: req.params.presentClassId}, (err, delA) => {
+        //   console.log(delA);
+        // });
+        Attendance.deleteMany({classId: req.params.presentClassId}, (err, deletedAtt) => {
+          if(err){
+            console.log(err);
+          } else {
+            console.log(deletedAtt);
+            res.redirect("/home");
+          }
+        });  
       }
     }
   );
@@ -465,35 +499,9 @@ app.post("/:presentClassId/:presentBatchId/newStudent", (req, res) => {
           req.params.presentBatchId +
           "/attendance"
       );
-      
-      newStudent.save();
-      res.redirect("/"+req.params.presentClassId+'/'+req.params.presentBatchId+'/attendance');
-
     }
   });
 });
-
-app.post(
-  "/:presentClassId/:presentBatchId/:presentStudentId/deleteStudent",
-  (req, res) => {
-    Student.findOneAndDelete(
-      { _id: req.params.presentStudentId },
-      (err, deletedStudent) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.redirect(
-            "/" +
-              req.params.presentClassId +
-              "/" +
-              req.params.presentBatchId +
-              "/attendance"
-          );
-        }
-      }
-    );
-  }
-);
 
 app.post(
   "/:presentClassId/:presentBatchId/:presentStudentId/deleteStudent",
@@ -639,7 +647,7 @@ app.post('/:presentClassId/:presentBatchId/:presentStudentId/deleteStudent', (re
     if(err){
       console.log(err);
     } else {
-      Attendance.findOneAndDelete({stdId: req.params.presentStudentId}, (err, deletedAtt) => {
+      Attendance.deleteMany({stdId: req.params.presentStudentId}, (err, deletedAtt) => {
         if(err){
           console.log(err);
         } else {
